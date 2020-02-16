cf50ec015b37139393a94b1b0e52ca85405a94e8
Carlos Naranjo <carlosnaranjo@ciencias.unam.mx>
2020-02-16 13:06:42 -0600

    (modificacion) parseando los demas albums

--- MusicApp/Explorar.cs
+++ MusicApp/Explorar.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,12 +23,16 @@ namespace MusicApp
         {
             List<AlbumClass> lista = Functions.JsonParse<AlbumClass>(@"..\..\albums\albums.json");
             foreach (AlbumClass elem in lista)
-            {
-                System.IO.File.WriteAllText(@"..\..\WriteText.txt", elem.ToString());
+            {   
+                //Mostrar Nombres de los alubums
+                //using (StreamWriter sw = File.AppendText(@"..\..\WriteText.txt"))
+                //{
+                //    sw.WriteLine(elem.ToString());
+                //}
+                Image imagen = Image.FromFile($@"..\..\albums\images\{elem.title}\{elem.title}.jpg");
+                Object[] renglon = new Object[] {imagen,$"{elem.title}","17-17-17"};
+                Albums.Rows.Add(renglon);
             }
-            Image imagen = Image.FromFile(@"..\..\albums\images\A Millions Miles Away\A Millions Miles Away.jpg");
-            Object[] renglon = new Object[] {imagen,"A Millions Miles Away","17-17-17"};
-            Albums.Rows.Add(renglon);
         }   
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
