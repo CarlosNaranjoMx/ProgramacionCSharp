96049b8b4151de607806020cb11968d18090f422
Carlos Naranjo <carlosnaranjo@ciencias.unam.mx>
2020-02-16 00:54:14 -0600

    (modificación) poniendole nombres a todo

---
+++ MusicApp/Inicio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicApp
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Busqueda frm2 = new Busqueda();
+            frm2.Show();
+        }
+    }
+}
---
+++ MusicApp/albums/windows2.ps1
@@ -0,0 +1,40 @@
+# $headers=@{}
+# $headers.Add("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com")
+# $headers.Add("x-rapidapi-key", "9df5865a80msh8eb11f35115ec57p1af4e4jsn60182eec8a44")
+# $response = Invoke-RestMethod -Uri 'https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem' -Method GET -Headers $headers
+# $response | ConvertTo-Json > artista.json
+
+# $list = "212377","100323","12047960","54101842","86511","301752","304193","75621062"
+$list = "103798792","105974222","58574292"
+function Download-Album {
+    param ($id_album)
+    $headers=@{}
+    $headers.Add("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com")
+    $headers.Add("x-rapidapi-key", "9df5865a80msh8eb11f35115ec57p1af4e4jsn60182eec8a44")
+    $response = Invoke-RestMethod -Uri "https://deezerdevs-deezer.p.rapidapi.com/album/$id_album" -Method GET -Headers $headers
+    
+    #Creacion del directorio
+    New-Item $response.title -itemtype Directory -Force
+    #Directorio de salida
+    $output = "C:\Users\cnaranjo\TODO\02-github\ProgramacionCSharp\musicapp02\$($response.title)\"
+    New-Item "$output\previews" -itemtype Directory -Force
+    ################################################################################
+    # Toda la información
+    $response | ConvertTo-Json > "$output$($response.title).json"
+    # Solo una parte de la info
+    # $response | Select-Object -Property id,title,release_date | ConvertTo-Json > "$($output)$($response.title).json"
+    ################################################################################
+    # Generos del album
+    $response.genres.data | ConvertTo-Json > "$($output)genres_$($response.title).json"
+    # Imagen del album
+    Invoke-WebRequest -Uri $response.cover_big -OutFile "$output$($response.title).jpg"
+    # Canciones del album
+    Invoke-WebRequest -Uri $response.tracklist -OutFile "$($output)canciones_$($response.title).json" 
+    
+    $canciones = $response.tracks.data | Select-Object -Property title,preview
+    $canciones | % {
+        Invoke-WebRequest -Uri $_.preview -OutFile "$($output)\previews\$($_.title).mp3" 
+    }
+}
+
+$list | % { Download-Album $_}
