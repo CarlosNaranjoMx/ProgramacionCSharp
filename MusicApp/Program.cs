80e7fa752b3126eb4be1ae76b8cdc9ca687c5f01
Arturo Castillo <acv629@gmail.com>
2020-03-04 00:09:53 -0600

    Deserialized all album information into a global object

--- MusicApp/Program.cs
+++ MusicApp/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using System.Text.Json;
 
 namespace MusicApp
 {
     static class Program
     {
+        static List<Album> albumes;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            string jsonString = File.ReadAllText("tracks.json");
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            albumes = JsonSerializer.Deserialize<List<Album>>(jsonString, options);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Inicio());
