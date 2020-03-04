6fc5b2ec5362f9e0b0a778dbc973d7a78267d948
Arturo Castillo <acv629@gmail.com>
2020-03-03 23:38:49 -0600

    Created JSON deserialize objects

---
+++ MusicApp/Track.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp
+{
+    class Track
+    {
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public int Track_Position { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
